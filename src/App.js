ae95c6252b2274c7fd594adb0b4a8ca61500d20f
Oleksandr Lozenko <131644673+OleksandrLozenko@users.noreply.github.com>
2024-12-02 15:26:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleTurnOn = () => {
+    fetch('http://localhost:8000/turn_on')
+      .then(response => response.json())
+      .then(data => console.log("Response from backend:", data))
+      .catch(error => console.error('Error:', error));
+  };
+
+  const handleTurnOff = () => {
+    fetch('http://localhost:8000/turn_off')
+      .then(response => response.json())
+      .then(data => console.log("Response from backend:", data))
+      .catch(error => console.error('Error:', error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => console.log('Light ON')}>Включить лампочку</button>
-        <button onClick={() => console.log('Light OFF')}>Выключить лампочку</button>
+        <button onClick={handleTurnOn}>Включить лампочку</button>
+        <button onClick={handleTurnOff}>Выключить лампочку</button>
       </header>
     </div>
   );
